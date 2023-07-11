b7458928093673bb397a70e7eb345f985ea44de4
QAAAK <dmitsantalov@yandex.ru>
2023-07-11 14:28:54 +0300

    version 1.0

---
+++ search_alpha.py
@@ -0,0 +1,25 @@
+
+def search4vowels(phrase: str):
+
+    """Returns the set of vowels found in 'phrase'."""
+    return set('aeiou').intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str='aeiou'):
+
+    """Returns the set of 'letters' found in 'phrase'."""
+    return set(letters).intersection(set(phrase))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
