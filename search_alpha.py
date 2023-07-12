9628753aa1bf9db5d1c7d5969855cc106ba3888d
QAAAK <dmitsantalov@yandex.ru>
2023-07-12 12:16:07 +0300

    version 1.1.0

--- search_alpha.py
+++ search_alpha.py
@@ -10,7 +10,9 @@ def search4letters(phrase: str, letters: str='aeiou'):
     """Returns the set of 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
 
-
+def log_request(req, res):
+    with open('vsearch.log', 'a') as vs:
+        print(req,res,file=vs)
 
 
 
--- vsearch.py
+++ vsearch.py
@@ -1,23 +1,32 @@
 from flask import Flask, render_template, request
+from markupsafe import escape
 from search_alpha import *
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello world from Flask!'
 @app.route('/search4', methods=['POST'])
 def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_title='Here are your results: ',
                             the_letters=letters, the_phrase=phrase, the_results=results)
+
+
+@app.route('/')
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 
-app.run()
+@app.route('/viewlog')
+def view_log():
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return escape(contents)
+
+if __name__ == '__main__':
+    app.run(debug=True)
